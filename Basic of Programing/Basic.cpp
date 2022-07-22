676c7dedb853dce89b083f9bab096a0311673d67
AL NAFIS FUAD SHUVO <88796714+shuvonafis66@users.noreply.github.com>
2022-07-23 01:27:01 +0530

    Add files via upload

---
+++ Basic of Programing/Basic.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+int a=72;
+
+if(a>=90)
+{
+    cout<<"0 grade";
+
+}
+else if(a>80)
+{
+    cout<<"E grade";
+}
+
+else if(a>70)
+{
+    cout<<"A grade";
+}
+
+else if(a>60)
+{
+    cout<<"B grade";
+}
+return 0;
+}
